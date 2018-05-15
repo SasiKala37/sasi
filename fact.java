b20c43d108328f16c8fed29102383fb72e331378
bridgelabz <SasiKala37>
2018-05-15 16:12:32 +0530

    fact added to master

---
+++ fact.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class fact
+{
+ public static void main (String args[])
+ {
+   int fat=1;
+	for(int i=1;i<5;i++)
+	{
+	  fat=fat*i;
+	}
+ 	System.out.println(fat);
+ }
+}
