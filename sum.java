9025dccd453eb477412d4953e3dac6a020e2b9ac
bridgelabz <SasiKala37>
2018-05-15 14:15:58 +0530

    sum file added

---
+++ sum.java
@@ -0,0 +1,17 @@
+import java.util.Sacnner;
+public class sump
+ {
+	public static void main(String args[])
+	{
+		int num1;
+		int num2;
+		int sum;
+		Scanner sc =new Scanner(System.in); 
+		System.out.println("Enter num1");
+			num1=sc.nextInt();
+		System.out.println("Enter num2");
+		num2=sc.nextInt();
+		sum=num1+num2l;
+		System.out.println("Sum is"+sum);
+	}
+ }
